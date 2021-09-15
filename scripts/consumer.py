7bba5778593e89548a8538905f0239bb782aee57
Fumbani Banda <60394916+deadex-ng@users.noreply.github.com>
2021-09-15 09:02:17 +0200

    Add doc strings

--- scripts/consumer.py
+++ scripts/consumer.py
@@ -3,13 +3,14 @@ from json import dumps
 from kafka import KafkaConsumer
 from json import loads
 
+"""Set up Kafka Consumer"""
 consumer = KafkaConsumer('group6_test',
 client_id='d_id',
 bootstrap_servers=['b-1.demo-cluster-1.9q7lp7.c1.kafka.eu-west-1.amazonaws.com:9092'],
 auto_offset_reset='earliest',
 enable_auto_commit=False)
 
-
+"""Dispaly values in topic"""
 for event in consumer:
     event_data = event.value
     # Do whatever you want
